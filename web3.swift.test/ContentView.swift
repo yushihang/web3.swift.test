576a8651aa25e43f57620b1dd62a59f841efb1ce
yushihang <shihang.yu@hsbc.com>
2024-07-17 15:25:48 +0800

    no message

---
+++ web3.swift.test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  web3.swift.test
+//
+//  Created by yushihang on 2024/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ web3.swift.test/web3_swift_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  web3_swift_testApp.swift
+//  web3.swift.test
+//
+//  Created by yushihang on 2024/7/17.
+//
+
+import SwiftUI
+
+@main
+struct web3_swift_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
